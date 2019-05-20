26efc8855c5a7ca52a6dc782498a883247bc2c03
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-20 20:37:30 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,8 +10,8 @@ def argue(phrase = "I'm right and you are wrong!")
   return phrase
 end
 
-def greeting
-
+def greeting(greeting = "Hi there, ", name = "Bobby!")
+  return "#{greeting}#{name}"
 end
 
 def return_a_value
